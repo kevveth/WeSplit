e8c44b62ae98eb6dba25ea5cc1a1680cb48ab6f5
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-01-30 14:42:32 -0800

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Kenneth Oliver Rathbun on 1/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+                
+                Section("How much would you like to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section{
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
